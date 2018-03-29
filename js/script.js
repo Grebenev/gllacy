0d212f9a055ae16b073fb9f6ccba7dc988988aba
Konstantin Grebenev <3304040@gmail.com>
2018-03-29 14:08:39 +0300

    Добавляет интерактивную карту

--- js/script.js
+++ js/script.js
@@ -9,5 +9,30 @@ button_click.addEventListener("click", function(evt) {
 feedback_close.addEventListener("click", function(evt) {
   evt.preventDefault();
   feedback.classList.remove("modal__overlay--on");
-
 });
+
+ymaps.ready(init);
+
+function init() {
+  var myMap = new ymaps.Map("map", {
+      center: [59.938631, 30.323055],
+      zoom: 15
+    }),
+
+    myPlacemark1 = new ymaps.Placemark([59.938631, 30.323055], {
+      balloonContent: "ул. Большая Конюшенная 19/8, Санкт-Петербург"
+    }, {
+      iconLayout: "default#image",
+      iconImageClipRect: [
+        [0, 0],
+        [80, 140]
+      ],
+      iconImageHref: "/img/pin.svg",
+      iconImageSize: [80, 140],
+      iconImageOffset: [-40, -90],
+    });
+
+  myMap.geoObjects.add(myPlacemark1)
+    .add(myPlacemark2)
+    .add(myPlacemark3);
+}
